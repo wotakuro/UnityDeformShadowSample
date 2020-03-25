461b2a59037655445967094a9577d43511e8d6cc
Yusuke Kurokawa <wotakuro@gmail.com>
2020-03-26 02:29:30 +0900

    update

---
+++ Assets/Samples/ShadowParameterUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShadowParameterUpdate : MonoBehaviour
+{
+    public DeformShadow deformShadow;
+    public Light light;
+    public Transform ground;
+
+    public void Update()
+    {
+        deformShadow.lightDir = light.transform.forward;
+        deformShadow.groundOffsetY = ground.position.y + 0.01f;
+    }
+}
